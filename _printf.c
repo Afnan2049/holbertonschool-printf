024ec8c4506ff2336304de9fc4b047b8023065e4
Hanin157 <alshaneen856@gmail.com>
2025-11-19 20:25:22 +0000

    Task 0: basic printf with c, s, %

--- _printf.c
+++ _printf.c
@@ -1,35 +1,52 @@
 #include "main.h"
 
 /**
- * _printf - reproduces basic behavior of printf
- * @format: format string
+ * _printf - produces output according to a format
+ * @format: the format string
+ *
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
 va_list args;
-int i = 0, count = 0;
+int i;
+int count;
 
 if (format == NULL)
+{
 return (-1);
+}
 
 va_start(args, format);
+i = 0;
+count = 0;
 
-while (format[i])
+while (format[i] != '\0')
 {
 if (format[i] == '%')
 {
 i++;
-if (format[i] == 'c')
-count += print_char(args);
-else if (format[i] == 's')
-count += print_string(args);
-else if (format[i] == '%')
-count += write(1, "%", 1);
-else
+if (format[i] == '\0')
 {
 va_end(args);
 return (-1);
+}
+if (format[i] == 'c')
+{
+count += print_char(args);
+}
+else if (format[i] == 's')
+{
+count += print_string(args);
+}
+else if (format[i] == '%')
+{
+count += write(1, "%", 1);
+}
+else
+{
+count += write(1, "%", 1);
+count += write(1, &format[i], 1);
 }
 }
 else
