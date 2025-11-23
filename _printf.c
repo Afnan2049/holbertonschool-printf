a13270314b57ee97a0feb32b459e041325d1eea4
root <alfaidi.afnan@gmail.com>
2025-11-23 14:18:23 +0000

    minimum call write by using local buffer of 1024

--- _printf.c
+++ _printf.c
@@ -11,6 +11,7 @@ int _printf(const char *format, ...)
 va_list args;
 int i = 0;
 int count = 0;
+
 char buffer[1024];
 int buf_index = 0;
 
@@ -67,42 +68,11 @@ buf_index = 0
 }
 i++;
 }
+
 if (buf_index > 0)
 {
 write(1, buffer, buf_index);
 count += buf_index;
-}
-{
-count += write(1, "%", 1);
-}
-
-else if (format[i] == 'u')
-{
-count += print_unsigned(args);
-}
-else if (format[i] == 'o')
-{
-count += print_octal(args);
-}
-else if (format[i] == 'x')
-{
-count += print_hex_lower(args);
-}
-else if (format[i] == 'X')
-{
-count += print_hex_upper(args);
-}
-else
-{
-count += write(1, "%", 1);
-count += write(1, &format[i], 1);
-}
-}
-else
-{
-count += write(1, &format[i], 1);
-}
-i++;
 }
 
 va_end(args);
